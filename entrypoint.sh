65e3fdb7965dedf6b6b9e18689839b83a3095444
Julian Haupt <julian.haupt@hauptmedia.de>
2018-07-17 19:59:05 +0200

    drop privileges but do not change ownership of files

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,4 +31,19 @@ if [ "${ELASTICSEARCH_ENABLED}" == "false" ] || [ "${APPLICATION_MODE}" == "mirr
     ARGS="--no-search ${ARGS}"
 fi
 
-exec "${BITBUCKET_INSTALL_DIR}/bin/start-bitbucket.sh" ${ARGS}
+# Start Bitbucket as the correct user.
+if [ "${UID}" -eq 0 ]; then
+#    echo "User is currently root. Will change directory ownership to ${RUN_USER}:${RUN_GROUP}, then downgrade permission to ${RUN_USER}"
+#    PERMISSIONS_SIGNATURE=$(stat -c "%u:%U:%a" "${BITBUCKET_HOME}")
+#    EXPECTED_PERMISSIONS=$(id -u ${RUN_USER}):${RUN_USER}:700
+#    if [ "${PERMISSIONS_SIGNATURE}" != "${EXPECTED_PERMISSIONS}" ]; then
+#        echo "Updating permissions for BITBUCKET_HOME"
+#        mkdir -p "${BITBUCKET_HOME}/lib" &&
+#            chmod -R 700 "${BITBUCKET_HOME}" &&
+#            chown -R "${RUN_USER}:${RUN_GROUP}" "${BITBUCKET_HOME}"
+#    fi
+    # Now drop privileges
+    exec su -s /bin/bash "${RUN_USER}" -c "${BITBUCKET_INSTALL_DIR}/bin/start-bitbucket.sh ${ARGS}"
+else
+    exec "${BITBUCKET_INSTALL_DIR}/bin/start-bitbucket.sh" ${ARGS}
+fi
